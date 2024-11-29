2ef94e7bdaba10bfedf0be44b0a69503cc5a536c
victor <ortinsv1@gmail.com>
2024-11-29 13:16:53 -0300

    New utils functions to easier calculus

--- src/calculations/utils.py
+++ src/calculations/utils.py
@@ -1,16 +1,60 @@
-from scipy.stats import spearmanr
-from scipy.stats import kendalltau
+import os
+import sys
 
-def spearman_corr(partial_standings, final_standings, alternative='two-sided'):
-    corr, p_value = spearmanr(partial_standings, final_standings, alternative=alternative)
-    return corr, p_value
+import pandas as pd
 
-def normalized_tau_distance(partial_standings, final_standings, complete_return: bool = False):
-    tau_corr, _ = kendalltau(partial_standings, final_standings)
-    tau_distance = ((1 - tau_corr)*(len(partial_standings)*(len(partial_standings) - 1)))/4
-    normalized_tau_distance = 2*tau_distance/(len(partial_standings)*(len(partial_standings) - 1))
+project_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+sys.path.append(project_path)
 
-    if complete_return:
-        return normalized_tau_distance, tau_distance, tau_corr
+from src.calculations.corr import spearman_corr, normalized_tau_distance  # noqa: E402
+from src.calculations.table_generation import generate_table  # noqa: E402
+
+
+def spearman_tau_table(
+    year_table: pd.DataFrame,
+    return_as_list: bool = False
+):
+
+    plot_points_corr = []
+    plot_points_tau = []
+
+    for i in range(1, 39):
+        rho, _ = spearman_corr(year_table[f"{i}"].to_list(), year_table["38"].to_list())
+        plot_points_corr.append(rho)
+
+        norm_tau_distance = normalized_tau_distance(
+            year_table[f"{i}"].to_list(), year_table["38"].to_list()
+        )
+        plot_points_tau.append(norm_tau_distance)
+
+
+    if return_as_list:
+        return plot_points_corr, plot_points_tau
     
-    return normalized_tau_distance
+    table_df = pd.DataFrame([plot_points_corr, plot_points_tau], columns=[i for i in range(1, 39)], index=['Spearman', 'Tau'])
+
+    return table_df
+
+def generate_spearman_tau_22_seasons():
+    spearmans_list = []
+    taus_list = []
+
+    for i in range(1, 23):
+        rank_table_df = generate_table(1.325, random_seed=i)
+        spearman_list, tau_list = spearman_tau_table(rank_table_df, return_as_list=True)
+
+        spearmans_list.append(spearman_list)
+        taus_list.append(tau_list)
+
+    sum_spearmans_list = [0]*len(spearmans_list[0])
+    sum_taus_list = [0]*len(taus_list[0])
+
+    for i in range(0, len(spearmans_list[0])):
+        for spearman_list, tau_list in zip(spearmans_list, taus_list):
+            sum_spearmans_list[i] += spearman_list[i]
+            sum_taus_list[i] += tau_list[i]
+
+    mean_spearmans_list = [sum_num/22 for sum_num in sum_spearmans_list]
+    mean_taus_list = [sum_num/22 for sum_num in sum_taus_list]
+
+    return mean_spearmans_list, mean_taus_list
