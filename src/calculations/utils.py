d5dcb80af640876f64397f0b381f79b14b9d3ee0
victor <ortinsv1@gmail.com>
2024-11-30 16:32:30 -0300

    Avg tau and spearman from multiple seasons per round, and typing and docs

--- src/calculations/utils.py
+++ src/calculations/utils.py
@@ -3,6 +3,8 @@ import sys
 
 import pandas as pd
 
+from typing import List, Tuple, Union
+
 project_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
 sys.path.append(project_path)
 
@@ -11,9 +13,20 @@ from src.calculations.table_generation import generate_table  # noqa: E402
 
 
 def spearman_tau_table(
-    year_table: pd.DataFrame,
-    return_as_list: bool = False
-):
+    year_table: pd.DataFrame, return_as_list: bool = False
+) -> pd.DataFrame:
+    """
+    Computes the Spearman correlation and normalized Kendall-tau distance for a given season's rankings.
+
+    Parameters:
+        year_table (pd.DataFrame): The table containing the rankings of teams over different matchweeks.
+        return_as_list (bool, optional): If True, returns the results as lists;
+            otherwise, returns them as a DataFrame. Default is False.
+
+    Returns:
+        pd.DataFrame or tuple: The Spearman correlations and normalized Kendall-tau distances either as a DataFrame
+            or as separate lists, based on the `return_as_list` flag.
+    """
 
     plot_points_corr = []
     plot_points_tau = []
@@ -27,34 +40,89 @@ def spearman_tau_table(
         )
         plot_points_tau.append(norm_tau_distance)
 
-
     if return_as_list:
         return plot_points_corr, plot_points_tau
-    
-    table_df = pd.DataFrame([plot_points_corr, plot_points_tau], columns=[i for i in range(1, 39)], index=['Spearman', 'Tau'])
+
+    table_df = pd.DataFrame(
+        [plot_points_corr, plot_points_tau],
+        columns=[i for i in range(1, 39)],
+        index=["Spearman", "Tau"],
+    )
 
     return table_df
 
-def generate_spearman_tau_22_seasons():
+
+def generate_spearman_tau(num_seasons: int = 22) -> Tuple[List[float], List[float]]:
+    """
+    Generates the average Spearman correlation and normalized Kendall-tau distance over a number of seasons.
+
+    Parameters:
+        num_seasons (int, optional): The number of seasons to simulate. Default is 22.
+
+    Returns:
+        tuple: The mean Spearman correlation and mean normalized Kendall-tau distance across all simulated seasons.
+    """
     spearmans_list = []
     taus_list = []
 
-    for i in range(1, 23):
+    for i in range(1, num_seasons + 1):
         rank_table_df = generate_table(1.325, random_seed=i)
         spearman_list, tau_list = spearman_tau_table(rank_table_df, return_as_list=True)
 
         spearmans_list.append(spearman_list)
         taus_list.append(tau_list)
 
-    sum_spearmans_list = [0]*len(spearmans_list[0])
-    sum_taus_list = [0]*len(taus_list[0])
+    return spearman_tau_mean(taus_list, spearmans_list)
+
+
+def spearman_tau_from_tables(years_table_list: list) -> Tuple[List[float], List[float]]:
+    """
+    Computes the average Spearman correlation and normalized Kendall-tau distance from a list of ranking tables.
+
+    Parameters:
+        years_table_list (list): A list of ranking tables, one for each season.
+
+    Returns:
+        tuple: The mean Spearman correlation and mean normalized Kendall-tau distance across all seasons.
+    """
+    spearmans_list = []
+    taus_list = []
+
+    for rank_table_df in years_table_list:
+        spearman_list, tau_list = spearman_tau_table(rank_table_df, return_as_list=True)
+
+        spearmans_list.append(spearman_list)
+        taus_list.append(tau_list)
+
+    return spearman_tau_mean(taus_list, spearmans_list)
+
+
+def spearman_tau_mean(
+    taus_list: list, spearmans_list: list
+) -> Tuple[List[float], List[float]]:
+    """
+    Calculates the mean Spearman correlation and mean normalized Kendall-tau distance from multiple seasons.
+
+    Parameters:
+        taus_list (list): A list of normalized Kendall-tau distances for each season.
+        spearmans_list (list): A list of Spearman correlation coefficients for each season.
+
+    Returns:
+        tuple: The mean Spearman correlation and the mean normalized Kendall-tau distance across all seasons.
+    """
+
+    if len(taus_list) != len(spearmans_list):
+        return None
+
+    sum_spearmans_list = [0] * len(spearmans_list[0])
+    sum_taus_list = [0] * len(taus_list[0])
 
     for i in range(0, len(spearmans_list[0])):
         for spearman_list, tau_list in zip(spearmans_list, taus_list):
             sum_spearmans_list[i] += spearman_list[i]
             sum_taus_list[i] += tau_list[i]
 
-    mean_spearmans_list = [sum_num/22 for sum_num in sum_spearmans_list]
-    mean_taus_list = [sum_num/22 for sum_num in sum_taus_list]
+    mean_spearmans_list = [sum_num / (len(taus_list)) for sum_num in sum_spearmans_list]
+    mean_taus_list = [sum_num / (len(taus_list)) for sum_num in sum_taus_list]
 
     return mean_spearmans_list, mean_taus_list
